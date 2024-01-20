dc396e3dcb676b34fbe26a4e9dda5b06c1545bd9
Flavien Fuchs <flavien.fuchs@gmail.com>
2024-01-20 17:05:16 +0100

    ajout de la fonction de loading de l'API, ajout des APIs Weapons et Charms et quelques ajustement de design

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+# Monster Hunter World Builder
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Monster Hunter World Builder is a React application to try every build possible in the game Monster Hunter : World.
+Test every build possible in our future fighting game!
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Start intruduction :
+- git clone this-repertory-ssh
+- cd ./repertory-name
+- npm install
+- npm run dev
--- src/components/Armors.jsx
+++ src/components/Armors.jsx
@@ -1,40 +1,47 @@
 /* eslint-disable react/prop-types */
-import axios from 'axios'
-import { useState } from 'react'
 import '../css/Items.css'
 /* import ItemArmor from './ItemArmor'; */
 
 
 
-function Armors({ handleArmor, type, closePage }) {
+function Armors({ armors, handleArmor, type, closePage }) {
 
-    const [armors, setArmors] = useState(null);
 
-    axios
-        .get('https://mhw-db.com/armor')
-        .then((response) => {
-            let newArmors = []
-            response.data.map((item) => {
-                if (item.type === type) newArmors.push(item)
-            })
-            setArmors(newArmors)
-        })
+    let newArmors = []
+    armors.data.map((item) => {
+        if (item.type === type) newArmors.push(item)
+    })
 
 
     return (
         <>
-            {armors && (<div className="itemContainer">
-                <button onClick={closePage}>Close Page</button>
+            <div className="itemContainer">
+                <div className='itemNavBar'>
+                    <button onClick={closePage}>Close Page</button>
+                </div>
 
-                {armors.slice(0, 20).map((armor, key) => {
+                {newArmors.map((armor, key) => {
 
                     return (<div key={key} className="item" onClick={() => { handleArmor(armor, type) }} >
-                        <p>armor name: {armor.name}</p>
-                        <img src={armor.assets.imageMale} alt="armor" />
+                        <p>{armor.id} Armor Name: {armor.name}</p>
+                        <p>Rarity {armor.rarity}</p>
+                        {!armor.assets ? <img src="./src/images/nullArmor.png" alt="armor" /> : ((armor.assets.imageMale) ? <img src={armor.assets.imageMale} alt="armor" /> : <img src={armor.assets.imageFemale} alt="armor" />)}
+                        <p>Defense : base {armor.defense.base} | max{armor.defense.max} | augmented {armor.defense.augmented}</p>
+                        <ul className='resistance'>
+                            <li><img src="./src/images/icons/fire-icon.png" alt="fire" />Fire Resist : {armor.resistances.fire}</li>
+                            <li><img src="./src/images/icons/water-icon.png" alt="water" />Water Resist : {armor.resistances.water}</li>
+                            <li><img src="./src/images/icons/ice-icon.png" alt="ice" />Ice Resist : {armor.resistances.ice}</li>
+                            <li><img src="./src/images/icons/thunder-icon.png" alt="thunder" />Thunder Resist : {armor.resistances.thunder}</li>
+                            <li><img src="./src/images/icons/dragon-icon.png" alt="dragon" />Dragon Resist : {armor.resistances.dragon}</li>
+                        </ul>
+                        <p>skills list</p>
+                        <ul>
+                            {armor.skills.map((skill, key) => { return <li key={key}>{skill.skillName} - {skill.level}</li> })}
+                        </ul>
                     </div>)
                 })}
-            </div >)
-            }
+            </div >
+
         </>
     )
 
