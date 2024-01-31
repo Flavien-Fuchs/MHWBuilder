961b6007c37d224491d064b5c81126e522e4c0dd
kanna 77 <kannavija@gmail.com>
2024-01-31 11:05:46 +0100

    Design update

--- src/components/Armors.jsx
+++ src/components/Armors.jsx
@@ -24,56 +24,63 @@ function Armors({ armors, handleArmor, type, closePage }) {
                 handleArmor(armor, type);
               }}
             >
-              <p>{armor.name}</p>
-              <p className="rarity">Rarity {armor.rarity}</p>
-              {!armor.assets ? (
-                <img src="./src/images/nullArmor.png" alt="armor" />
-              ) : armor.assets.imageMale ? (
-                <img src={armor.assets.imageMale} alt="armor" />
-              ) : (
-                <img src={armor.assets.imageFemale} alt="armor" />
-              )}
-              <p className="stats">
-                Defense : base {armor.defense.base} | max{armor.defense.max} |
-                augmented {armor.defense.augmented}
-              </p>
-              <ul className="resistance">
-                <li>
-                  <img src="./src/images/icons/fire-icon.png" alt="fire" />
-                  Fire Resist : {armor.resistances.fire}
-                </li>
-                <li>
-                  <img src="./src/images/icons/water-icon.png" alt="water" />
-                  Water Resist : {armor.resistances.water}
-                </li>
-                <li>
-                  <img src="./src/images/icons/ice-icon.png" alt="ice" />
-                  Ice Resist : {armor.resistances.ice}
-                </li>
-                <li>
-                  <img
-                    src="./src/images/icons/thunder-icon.png"
-                    alt="thunder"
-                  />
-                  Thunder Resist : {armor.resistances.thunder}
-                </li>
-                <li>
-                  <img src="./src/images/icons/dragon-icon.png" alt="dragon" />
-                  Dragon Resist : {armor.resistances.dragon}
-                </li>
-              </ul>
-              {armor.skills && armor.skills.length > 0 ? (
-                <div>
-                  <p className="skillsList">skills list</p>
-                  <ul className="hideSkills">
-                    {armor.skills.map((skill, key) => (
-                      <li key={key}>
-                        {skill.skillName} - {skill.level}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
+              <div className="globalInfos">
+                <p className="paragrapheList">{armor.name}</p>
+                <p className="rarity">Rarity {armor.rarity}</p>
+                {!armor.assets ? (
+                  <img src="./src/images/nullArmor.png" alt="armor" />
+                ) : armor.assets.imageMale ? (
+                  <img src={armor.assets.imageMale} alt="armor" />
+                ) : (
+                  <img src={armor.assets.imageFemale} alt="armor" />
+                )}
+                <p className="stats">
+                  Defense : base {armor.defense.base} | max{armor.defense.max} |
+                  augmented {armor.defense.augmented}
+                </p>
+              </div>
+              <div className="detailsInfos">
+                <ul className="resistance">
+                  <li>
+                    <img src="./src/images/icons/fire-icon.png" alt="fire" />
+                    Fire Resist : {armor.resistances.fire}
+                  </li>
+                  <li>
+                    <img src="./src/images/icons/water-icon.png" alt="water" />
+                    Water Resist : {armor.resistances.water}
+                  </li>
+                  <li>
+                    <img src="./src/images/icons/ice-icon.png" alt="ice" />
+                    Ice Resist : {armor.resistances.ice}
+                  </li>
+                  <li>
+                    <img
+                      src="./src/images/icons/thunder-icon.png"
+                      alt="thunder"
+                    />
+                    Thunder Resist : {armor.resistances.thunder}
+                  </li>
+                  <li>
+                    <img
+                      src="./src/images/icons/dragon-icon.png"
+                      alt="dragon"
+                    />
+                    Dragon Resist : {armor.resistances.dragon}
+                  </li>
+                </ul>
+                {armor.skills && armor.skills.length > 0 ? (
+                  <div>
+                    <p className="skillsList">skills list</p>
+                    <ul className="hideSkills">
+                      {armor.skills.map((skill, key) => (
+                        <li key={key}>
+                          {skill.skillName} - {skill.level}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : null}
+              </div>
             </div>
           );
         })}
