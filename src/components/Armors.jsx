60538a3f9e4d0219313d489323262d02ca060c8b
AyouBattle <152877288+AyouBattle@users.noreply.github.com>
2024-02-04 19:07:37 +0100

    Update Armors.jsx

--- src/components/Armors.jsx
+++ src/components/Armors.jsx
@@ -3,8 +3,6 @@ import '../css/Items.css'
 import { useState } from 'react'
 /* import ItemArmor from './ItemArmor'; */
 
-
-
 function Armors({ armors, handleArmor, type, closePage }) {
   const [searchTerm, setSearchterm] = useState("")
   const [showBar, setShowBar] = useState(false)
@@ -40,31 +38,78 @@ function Armors({ armors, handleArmor, type, closePage }) {
                     ></input>)}
                 </div>
 
-                {newArmors.map((armor, key) => {
-
-                    return (<div key={key} className="item" onClick={() => { handleArmor(armor, type) }} >
-                        <p>{armor.id} Armor Name: {armor.name}</p>
-                        <p>Rarity {armor.rarity}</p>
-                        {!armor.assets ? <img src="./src/images/nullArmor.png" alt="armor" /> : ((armor.assets.imageMale) ? <img src={armor.assets.imageMale} alt="armor" /> : <img src={armor.assets.imageFemale} alt="armor" />)}
-                        <p>Defense : base {armor.defense.base} | max{armor.defense.max} | augmented {armor.defense.augmented}</p>
-                        <ul className='resistance'>
-                            <li><img src="./src/images/icons/fire-icon.png" alt="fire" />Fire Resist : {armor.resistances.fire}</li>
-                            <li><img src="./src/images/icons/water-icon.png" alt="water" />Water Resist : {armor.resistances.water}</li>
-                            <li><img src="./src/images/icons/ice-icon.png" alt="ice" />Ice Resist : {armor.resistances.ice}</li>
-                            <li><img src="./src/images/icons/thunder-icon.png" alt="thunder" />Thunder Resist : {armor.resistances.thunder}</li>
-                            <li><img src="./src/images/icons/dragon-icon.png" alt="dragon" />Dragon Resist : {armor.resistances.dragon}</li>
-                        </ul>
-                        <p>skills list</p>
-                        <ul>
-                            {armor.skills.map((skill, key) => { return <li key={key}>{skill.skillName} - {skill.level}</li> })}
-                        </ul>
-                    </div>)
-                })}
-            </div >
-
-        </>
-    )
-
+        {newArmors.map((armor, key) => {
+          return (
+            <div
+              key={key}
+              className="item"
+              onClick={() => {
+                handleArmor(armor, type);
+              }}
+            >
+              <div className="globalInfos">
+                <p className="paragrapheList">{armor.name}</p>
+                <p className="rarity">Rarity {armor.rarity}</p>
+                {!armor.assets ? (
+                  <img src="./src/images/nullArmor.png" alt="armor" />
+                ) : armor.assets.imageMale ? (
+                  <img src={armor.assets.imageMale} alt="armor" />
+                ) : (
+                  <img src={armor.assets.imageFemale} alt="armor" />
+                )}
+                <p className="stats">
+                  Defense : base {armor.defense.base} | max{armor.defense.max} |
+                  augmented {armor.defense.augmented}
+                </p>
+              </div>
+              <div className="detailsInfos">
+                <ul className="resistance">
+                  <li>
+                    <img src="./src/images/icons/fire-icon.png" alt="fire" />
+                    Fire Resist : {armor.resistances.fire}
+                  </li>
+                  <li>
+                    <img src="./src/images/icons/water-icon.png" alt="water" />
+                    Water Resist : {armor.resistances.water}
+                  </li>
+                  <li>
+                    <img src="./src/images/icons/ice-icon.png" alt="ice" />
+                    Ice Resist : {armor.resistances.ice}
+                  </li>
+                  <li>
+                    <img
+                      src="./src/images/icons/thunder-icon.png"
+                      alt="thunder"
+                    />
+                    Thunder Resist : {armor.resistances.thunder}
+                  </li>
+                  <li>
+                    <img
+                      src="./src/images/icons/dragon-icon.png"
+                      alt="dragon"
+                    />
+                    Dragon Resist : {armor.resistances.dragon}
+                  </li>
+                </ul>
+                {armor.skills && armor.skills.length > 0 ? (
+                  <div>
+                    <p className="skillsList">skills list</p>
+                    <ul className="hideSkills">
+                      {armor.skills.map((skill, key) => (
+                        <li key={key}>
+                          {skill.skillName} - {skill.level}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export default Armors
+export default Armors;
