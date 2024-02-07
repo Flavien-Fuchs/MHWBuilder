4aa8a665b8fbcd314c946ebc40baa704fee9f0bb
kanna 77 <kannavija@gmail.com>
2024-02-07 20:08:19 +0100

    i add button top but i can't place very well for see

--- src/components/Armors.jsx
+++ src/components/Armors.jsx
@@ -1,45 +1,87 @@
 /* eslint-disable react/prop-types */
-import '../css/Items.css'
+
+import "../css/Items.css";
+import BackToTop from "./BackToTopButton";
 
 function Armors({ armors, handleArmor, type, closePage }) {
+  let newArmors = [];
+  armors.map((item) => {
+    if (item.type === type) newArmors.push(item);
+  });
 
-    let newArmors = []
-    armors.map((item) => {
-        if (item.type === type) newArmors.push(item)
-    })
+  return (
+    <>
+      <div className="itemContainer">
+        <div className="itemNavBar">
+          <button onClick={closePage}>Close Page</button>
+        </div>
 
-    return (
-        <>
-            <div className="itemContainer">
-                <div className='itemNavBar'>
-                    <button onClick={closePage}>Close Page</button>
-                </div>
-
-                {newArmors.map((armor, key) => {
-
-                    return (<div key={key} className="item" onClick={() => { handleArmor(armor, type) }} >
-                        <p>{armor.id} Armor Name: {armor.name}</p>
-                        <p>Rarity {armor.rarity}</p>
-                        {!armor.assets ? <img src="./src/images/nullArmor.png" alt="armor" /> : ((armor.assets.imageMale) ? <img src={armor.assets.imageMale} alt="armor" /> : <img src={armor.assets.imageFemale} alt="armor" />)}
-                        <p>Defense : base {armor.defense.base} | max{armor.defense.max} | augmented {armor.defense.augmented}</p>
-                        <ul className='resistance'>
-                            <li><img src="./src/images/icons/fire-icon.png" alt="fire" />Fire Resist : {armor.resistances.fire}</li>
-                            <li><img src="./src/images/icons/water-icon.png" alt="water" />Water Resist : {armor.resistances.water}</li>
-                            <li><img src="./src/images/icons/ice-icon.png" alt="ice" />Ice Resist : {armor.resistances.ice}</li>
-                            <li><img src="./src/images/icons/thunder-icon.png" alt="thunder" />Thunder Resist : {armor.resistances.thunder}</li>
-                            <li><img src="./src/images/icons/dragon-icon.png" alt="dragon" />Dragon Resist : {armor.resistances.dragon}</li>
-                        </ul>
-                        <p>skills list</p>
-                        <ul>
-                            {armor.skills.map((skill, key) => { return <li key={key}>{skill.skillName} - {skill.level}</li> })}
-                        </ul>
-                    </div>)
+        {newArmors.map((armor, key) => {
+          return (
+            <div
+              key={key}
+              className="item"
+              onClick={() => {
+                handleArmor(armor, type);
+              }}
+            >
+              <p>
+                {armor.id} Armor Name: {armor.name}
+              </p>
+              <p>Rarity {armor.rarity}</p>
+              {!armor.assets ? (
+                <img src="./src/images/nullArmor.png" alt="armor" />
+              ) : armor.assets.imageMale ? (
+                <img src={armor.assets.imageMale} alt="armor" />
+              ) : (
+                <img src={armor.assets.imageFemale} alt="armor" />
+              )}
+              <p>
+                Defense : base {armor.defense.base} | max{armor.defense.max} |
+                augmented {armor.defense.augmented}
+              </p>
+              <ul className="resistance">
+                <li>
+                  <img src="./src/images/icons/fire-icon.png" alt="fire" />
+                  Fire Resist : {armor.resistances.fire}
+                </li>
+                <li>
+                  <img src="./src/images/icons/water-icon.png" alt="water" />
+                  Water Resist : {armor.resistances.water}
+                </li>
+                <li>
+                  <img src="./src/images/icons/ice-icon.png" alt="ice" />
+                  Ice Resist : {armor.resistances.ice}
+                </li>
+                <li>
+                  <img
+                    src="./src/images/icons/thunder-icon.png"
+                    alt="thunder"
+                  />
+                  Thunder Resist : {armor.resistances.thunder}
+                </li>
+                <li>
+                  <img src="./src/images/icons/dragon-icon.png" alt="dragon" />
+                  Dragon Resist : {armor.resistances.dragon}
+                </li>
+              </ul>
+              <p>skills list</p>
+              <ul>
+                {armor.skills.map((skill, key) => {
+                  return (
+                    <li key={key}>
+                      {skill.skillName} - {skill.level}
+                    </li>
+                  );
                 })}
-            </div >
-
-        </>
-    )
-
+              </ul>
+            </div>
+          );
+        })}
+        <BackToTop />
+      </div>
+    </>
+  );
 }
 
-export default Armors
+export default Armors;
---
+++ src/components/BackToTop.jsx
@@ -0,0 +1,12 @@
+import BackToTopButton from "./BackToTopButton";
+
+function BackToTop() {
+  return (
+    <>
+      <div className="App">
+        <BackToTopButton />
+      </div>
+    </>
+  );
+}
+export default BackToTop;
